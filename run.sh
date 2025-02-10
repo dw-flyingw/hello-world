0270c4b1ab508bc9bf634fcabf741db9c8a56eae
Dave Wright <dave.wright@gmail.com>
2025-02-10 12:02:11 -0800

    added conda environments

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-# install needed python libraries
-pip --no-warn-script-location install psutil py-cpuinfo GPUtil
+conda create -n terminal
+conda activate terminal
+pip install psutil py-cpuinfo GPUtil --no-warn-script-location
 python ./hello-world/main.py 
