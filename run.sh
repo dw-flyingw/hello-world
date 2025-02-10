bd3c721a3e9cb0450dc4b64655d302222efbe52f
Dave Wright <dave.wright@gmail.com>
2025-02-10 10:22:37 -0800

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-git clone  https://github.com/dw-flyingw/hello-world
-#pip install psutil py-cpuinfo gputil
-python3 ./hello-world/main.py 
-
-
+# install needed python libraries
+pip install psutil py-cpuinfo gputil
+python ./hello-world/main.py 
