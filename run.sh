b93f9c927800fa776acbf19ed92032a9dedc35c1
Dave Wright <dave.wright@gmail.com>
2025-02-12 14:38:53 -0800

    updated

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #pip install  psutil py-cpuinfo GPUtil pynvml --no-warn-script-location 
 #clear
-PATH=$PATH:~/.local/bin
 #python ./hello-world/hello-world.py 
-nvidia-smi
+#nvidia-smi
 #ld -v
 #export NGC_API_KEY="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"
 
-pip install -r requirements.txt --no-warn-script-location 
+
+pip install -r hello-world/requirements.txt
 PATH=$PATH:~/.local/bin
