3630cbb8e880d9507a1489bd5b6cc1b8cd0923db
Dave Wright <dave.wright@gmail.com>
2025-02-12 14:56:03 -0800

    updated

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ## On remote container
 git clone https://github.com/dw-flyingw/hello-world && sh hello-world/run.sh
+
+git clone https://github.com/dw-flyingw/hello-world
+create .env file at /home/dave.wright-hpe.com/dave-wright-hpe-5f2c6f61/.env 
+cd hello-world
+python test-endpoint.py 
--- test-endpoint.py
+++ test-endpoint.py
@@ -1,6 +1,30 @@
+
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
 # Load environment variables from the .env file
-load_dotenv()
+dotenv_path = ('/home/dave.wright-hpe.com/dave-wright-hpe-5f2c6f61/.env')
+load_dotenv(dotenv_path)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_api_base = os.getenv("OPENAI_API_BASE")
+
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+completion = client.completions.create(model="meta/llama-3.1-8b-instruct",
+                                      prompt="Panama is a")
+print("Completion result:", completion)
+
+
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+
+
